a72cd25e7c36b3b7274dabf5559355d5cfd2693d
yu1hpa <yu.1hpa@gmail.com>
2021-09-08 04:14:20 +0900

    refactor: split main.go to main and fetch files

--- main.go
+++ main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-    "io"
-    "log"
     "fmt"
     "time"
     "flag"
-    "net/http"
     "encoding/json"
 )
 
@@ -72,23 +69,3 @@ func main() {
     }
 }
 
-func fetch(url string) []byte {
-    client := http.Client{}
-    req, err := http.NewRequest(http.MethodGet, url, nil)
-    if err != nil {
-        log.Fatal(err)
-    }
-    req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36")
-
-    resp, err := client.Do(req)
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer resp.Body.Close()
-
-    body, err := io.ReadAll(resp.Body)
-    if err != nil {
-        log.Fatal(err)
-    }
-    return body
-}
